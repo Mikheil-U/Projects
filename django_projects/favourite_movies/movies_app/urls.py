7d552f639ef1b7e3c7fd445d85afaa3f9324915f
Mikheil-U <uglavamisho@gmail.com>
2023-12-12 17:08:10 -0500

    upload files for page design

--- django_projects/favourite_movies/movies_app/urls.py
+++ django_projects/favourite_movies/movies_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login_user, name='login_user'),
+    path('login/', views.user_login, name='login_user'),
     path('logout/', views.user_logout, name='logout_user'),
+    path('delete-movie/<int:pk>', views.delete_table_item_on_click, name='delete_movie'),
 
 ]
--- django_projects/favourite_movies/movies_app/views.py
+++ django_projects/favourite_movies/movies_app/views.py
@@ -10,7 +10,6 @@ from .models import Movie
 
 
 def index(request):
-
     return render(request, 'movies_app/base.html', {'movies': Movie.objects.all()})
 
 
@@ -21,20 +20,18 @@ def user_logout(request):
 
 
 def user_login(request):
-    if request.method == "POST":
-        pass
-
-
-def login_user(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
+
+            # authenticate the user
             user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('index')  # Redirect to a success page.
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')  # Redirect to a success page.
             else:
                 # Return an 'invalid login' error message.
                 return render(request, 'movies_app/login.html', {'form': form, 'error': 'Invalid username or password.'})
@@ -43,4 +40,10 @@ def login_user(request):
 
     return render(request, 'movies_app/login.html', {'form': form})
 
-
+@login_required
+def delete_table_item_on_click(request, pk: int):
+    """To remove a movie from movies list when 'Delete' button is clicked"""
+    movie = Movie.objects.get(id=pk)
+    movie.delete()
+    movie.delete()
+    return redirect(request, 'movies_app/base.html', {})
