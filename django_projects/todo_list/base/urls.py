bc5c8bf30c6ef581c0426d11a83e95b852c3a48e
Mikheil-U <uglavamisho@gmail.com>
2024-01-05 17:17:11 -0500

    Add list and detail views along with HTML files to render them.

--- django_projects/todo_list/base/urls.py
+++ django_projects/todo_list/base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
 ]
--- django_projects/todo_list/base/views.py
+++ django_projects/todo_list/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 
@@ -7,3 +8,13 @@ class TaskList(ListView):
     # This will look for modelname_list.html so make sure to define one
     model = Task
     template_name = 'base/task_list.html'  # optional, but I still like to include it
+    context_object_name = 'tasks'  # object_list is the default name Django uses for ListView CBV
+
+
+class TaskDetail(DetailView):
+    # DetailView simply returns detailed information about the item
+    # Define modelname_detail.html so DetailView can render the HTML.
+    # Or use any name for an HTML file, but override the default name by setting the template_name
+    model = Task
+    context_object_name = 'task'  # default name is object
+    template_name = 'base/task.html'  # override the default name
